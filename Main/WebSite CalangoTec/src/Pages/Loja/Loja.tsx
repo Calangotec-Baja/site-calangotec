4d238be26feadda9021434663cbaf12bb72bd1ac
Pedro Lima <121100935+PPedrinho@users.noreply.github.com>
2025-01-19 14:38:42 -0300

    v0.0.2
    cards oficiais (sem produtos)

--- Main/WebSite CalangoTec/src/Pages/Loja/Loja.tsx
+++ Main/WebSite CalangoTec/src/Pages/Loja/Loja.tsx
@@ -1,36 +1,79 @@
-import loja from '../../assets/loja.png'
+import loja from '../../assets/loja.png';
 import Header2 from "../../Elements/Header2";
-import cardLoja from "../../assets/cardLoja.png"
+import cardLoja2 from "../../assets/cardLoja.png";
 
 export default function Loja() {
     return (
         <>
-            <Header2/>
+            <Header2 />
             <main>
-                <img src={loja} alt="fotoLoja" className="w-full"/>
+                <img src={loja} alt="fotoLoja" className="w-full" />
             </main>
             <div className="w-full px-4 lg:px-20 flex flex-col justify-center items-center h-fit">
-                <div className="flex flex-col text-center text-black text-4xl font-semibold font-['Inter'] pt-16 mb-4 tracking-wide mb-10 gap-4">
-                    <p className="text-black font-semibold text-4xl mb-[-15px]">PRODUTOS DO CALANGUINHO</p>
+                <div className="flex flex-col text-center text-black text-4xl font-semibold font-['Inter'] pt-16 mb-4 tracking-wide gap-4">
+                    <p className="drop-shadow-[0_1px_2px_rgba(0,0,0,0.8)] text-black font-semibold text-4xl mb-[-15px]">PRODUTOS DO CALANGUINHO</p>
                     <p className="text-[#666666] font-normal text-lg pb-8">Venha dar uma olhada nos nossos produtos!</p>
 
-                    <div className="flex flex-wrap justify-center items-center gap-16">
-                        <img src={cardLoja} alt="cardLoja"/>
-                        <img src={cardLoja} alt="cardLoja"/>
-                        <img src={cardLoja} alt="cardLoja"/>
-                    </div>
-                    <div className="flex flex-wrap justify-center items-center gap-16">
-                        <img src={cardLoja} alt="cardLoja"/>
-                        <img src={cardLoja} alt="cardLoja"/>
-                        <img src={cardLoja} alt="cardLoja"/>
-                    </div>
+                    {/* Cards e Botões */}
+                    <div className="flex flex-wrap justify-center items-center gap-[90px] rounded-lg">
+                        {/* Card 1 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Uniforme Calango
+                                </button>
+                            </a>
+                        </div>
+                        {/* Card 2 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Comprar
+                                </button>
+                            </a>
+                        </div>
+                        {/* Card 3 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Comprar
+                                </button>
+                            </a>
+                        </div>
                     
+                        {/* Card 4 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Comprar
+                                </button>
+                            </a>
+                        </div>
+                        {/* Card 5 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Comprar
+                                </button>
+                            </a>
+                        </div>
+                        {/* Card 6 */}
+                        <div className="flex flex-col items-center">
+                            <img src={cardLoja2} alt="cardLoja" className="rounded-lg" />
+                            <a href="" target='_blank'>
+                                <button className="w-[308px] mt-4 bg-gray-100 border-2 border-black text-black text-lg px-6 py-2 rounded-md hover:bg-gray-400 transition">
+                                    Comprar
+                                </button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div className='w-[348px] h-[383px] border-[3px] border-black'>
-                <div className="w-[348px] h-[62px] border-[3px] border-black "> </div>
-            </div>
         </>
-    )
+    );
 }
