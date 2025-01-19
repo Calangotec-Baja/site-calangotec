c1ff8b9ba2b00c316540298f0c333f52c0418198
Yan Chagas <yanchagas2004@gmail.com>
2025-01-19 02:48:16 -0300

    v0.1.1

--- Main/WebSite CalangoTec/src/Pages/Subsistema/Eletronica/Eletronica.tsx
+++ Main/WebSite CalangoTec/src/Pages/Subsistema/Eletronica/Eletronica.tsx
@@ -8,7 +8,7 @@ export default function Eletronica() {
             <div className="flex flex-col gap-16 items-center">
                 <img src={bgeletronica} alt="" className="w-full"/>
                 <div className="w-full flex flex-row justify-start">    
-                    <div className="flex flex-col text-white justify-center items-center bg-gray-800 py-2 sm:py-4 px-8 rounded-r-full text-xl sm:text-2xl font-[Inter] font-bold">
+                    <div className="flex flex-col text-white justify-center text-end items-center bg-gray-800 py-2 sm:py-4 px-8 rounded-br-full rounded-tr-2xl text-xl sm:text-2xl font-[Inter] font-bold w-1/4">
                         Sobre o susbistema:
                     </div>
                 </div>
